fbf4264e93c9f541c6c76634ba90e8194612f5c1
zhouphy5 <zhouphy5@gmail.com>
2024-03-07 14:07:49 +0800

    updatge

--- src/main/scala/org/example/SparkSessionTest.scala
+++ src/main/scala/org/example/SparkSessionTest.scala
@@ -2,23 +2,29 @@ package org.example
 
 import org.apache.spark.sql.SparkSession
 
+object SparkSessionTest {
+  def initSpark(): SparkSession = {
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("SparkByExample")
+      .config("spark.sql.broadcastTimeout", "1200")
+      .config("spark.hadoop.hive.exec.dynamic.partition", "true")
+      .config("spark.hadoop.hive.exec.dynamic.partition.mode", "nonstrict")
+      .config("spark.hadoop.hive.exec.max.dynamic.partitions", "4000")
+      .config("spark.sql.storeAssignmentPolicy", "legacy")
+      .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
+      .config("hive.metastore.uris", "thrift://hadoop73:9083")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    spark
+  }
 
-object SparkSessionTest extends App {
-  val spark: SparkSession = SparkSession.builder()
-    .master("local[1]").appName("SparkByExamples.com")
-    .getOrCreate()
-
-  import spark.implicits._
-
-  val columns = Seq("language", "users_count")
-  val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
-
-  // Spark Create DataFrame from RDD
-  val rdd = spark.sparkContext.parallelize(data)
-
-  // Create DataFrame
-  val dfFromRDD1 = rdd.toDF()
-  dfFromRDD1.show()
-
+  def main(args: Array[String]): Unit = {
+    val spark = initSpark()
+    val sql = "select * from sgami_bjzx_gate.a_gate_archive_new limit 2"
+    val df = spark.sql(sql)
+    df.show()
+  }
 
 }
